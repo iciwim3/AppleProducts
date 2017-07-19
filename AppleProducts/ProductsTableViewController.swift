63c28ce82b42f7df561aa40b721401d3ad907483
SKE <appybuildmore@gmail.com>
2017-07-19 12:07:45 -0500

    1. Updated the 'ProductsTableViewController' with new data to show all of the product lines. 2. Updated UITableViewDataSource methods. 3. Added section titles.

--- AppleProducts/ProductsTableViewController.swift
+++ AppleProducts/ProductsTableViewController.swift
@@ -16,7 +16,9 @@ class ProductsTableViewController: UITableViewController {
 
     // MARK: - Data model
     
-    var products = ProductLine.getProductLines()[1].products
+    // var products = ProductLine.getProductLines()[1].products
+    
+    var productLines: [ProductLine] = ProductLine.getProductLines()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,22 +33,32 @@ class ProductsTableViewController: UITableViewController {
     // MARK: - UITableViewDataSource
     
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return productLines.count
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return products.count
+        return productLines[section].products.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ProductTableViewCell
-        
+        let productLine = productLines[indexPath.section]
+        let products = productLine.products
         let product = products[indexPath.row]
         
         cell.product = product
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        let productLine = productLines[section]
+        
+        return productLine.name
+        
+        
     }
     
 }
